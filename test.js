417e815dfecb052ca2091b6e49437ce6ff24e1a4
stanleynephi <nul23001@byui.edu>
2023-10-28 16:12:02 +0000

    week one assignement

---
+++ test.js
@@ -0,0 +1,4 @@
+//add a hellow world to my test.html using javascript.
+const myheading = document.querySelector("h5");
+// call the variable name.textContent and then add the content you have to add.
+myheading.textContent = "Hello World";
